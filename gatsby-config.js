f35824a6b8ef63aaa937af887c5acf79b79fae4a
Hidekazu <tanakahidek@gmail.com>
2021-04-25 18:56:33 +0900

    fix setting

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,15 +3,12 @@ module.exports = {
     language: `en`, // or `ja`
     title: `gatsby-crudzoo demo site`,
     author: `your name`,
-    job: `Engineer`,
     keywords: [`demo`, `gatsby-theme`],
     heroText: `My Blog Site`,
     description: `site description`,
-    siteUrl: `http://example.com`,
     social: {
       twitter: `your twitter name`,
     },
-    algoliaSearch: false,
   },
   plugins: [
     {
